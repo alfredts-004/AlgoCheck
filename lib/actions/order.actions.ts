907eec858524e6c85ee91852a318c779ebbe4532
alfred-004 <113010988+alfred-004@users.noreply.github.com>
2024-03-17 13:11:02 +0530

    Removed Stripe

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,6 +1,5 @@
 "use server"
 
-import Stripe from 'stripe';
 import { CheckoutOrderParams, CreateOrderParams, GetOrdersByEventParams, GetOrdersByUserParams } from "@/types"
 import { redirect } from 'next/navigation';
 import { handleError } from '../utils';
@@ -10,55 +9,55 @@ import Event from '../database/models/event.model';
 import {ObjectId} from 'mongodb';
 import User from '../database/models/user.model';
 
-export const checkoutOrder = async (order: CheckoutOrderParams) => {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+// export const checkoutOrder = async (order: CheckoutOrderParams) => {
+//   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
-  const price = order.isFree ? 0 : Number(order.price) * 100;
+//   const price = order.isFree ? 0 : Number(order.price) * 100;
 
-  try {
-    const session = await stripe.checkout.sessions.create({
-      line_items: [
-        {
-          price_data: {
-            currency: 'usd',
-            unit_amount: price,
-            product_data: {
-              name: order.eventTitle
-            }
-          },
-          quantity: 1
-        },
-      ],
-      metadata: {
-        eventId: order.eventId,
-        buyerId: order.buyerId,
-      },
-      mode: 'payment',
-      success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
-      cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
-    });
+//   try {
+//     const session = await stripe.checkout.sessions.create({
+//       line_items: [
+//         {
+//           price_data: {
+//             currency: 'usd',
+//             unit_amount: price,
+//             product_data: {
+//               name: order.eventTitle
+//             }
+//           },
+//           quantity: 1
+//         },
+//       ],
+//       metadata: {
+//         eventId: order.eventId,
+//         buyerId: order.buyerId,
+//       },
+//       mode: 'payment',
+//       success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
+//       cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
+//     });
 
-    redirect(session.url!)
-  } catch (error) {
-    throw error;
-  }
-}
+//     redirect(session.url!)
+//   } catch (error) {
+//     throw error;
+//   }
+// }
 
-export const createOrder = async (order: CreateOrderParams) => {
-  try {
-    await connectToDatabase();
+// export const createOrder = async (order: CreateOrderParams) => {
+//   try {
+//     await connectToDatabase();
     
-    const newOrder = await Order.create({
-      ...order,
-      event: order.eventId,
-      buyer: order.buyerId,
-    });
+//     const newOrder = await Order.create({
+//       ...order,
+//       event: order.eventId,
+//       buyer: order.buyerId,
+//     });
 
-    return JSON.parse(JSON.stringify(newOrder));
-  } catch (error) {
-    handleError(error);
-  }
-}
+//     return JSON.parse(JSON.stringify(newOrder));
+//   } catch (error) {
+//     handleError(error);
+//   }
+// }
 
 // GET ORDERS BY EVENT
 export async function getOrdersByEvent({ searchString, eventId }: GetOrdersByEventParams) {
